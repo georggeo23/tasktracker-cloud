bc8bbd2a65ab12cffab1a8b7c017649a08782246
georggeo23 <georgechris2003@gmail.com>
2025-05-13 12:10:46 -0400

    Initializing Flask App

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from .config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    CORS(app)
+
+    from app.routes import auth, tasks
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(tasks.bp)
+
+    return app
